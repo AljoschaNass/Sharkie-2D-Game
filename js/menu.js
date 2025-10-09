6156585c78095aac6f57cecf23d1fb6839ebc317
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-10-09 17:46:52 +0200

    feat: startScreen Shark with swim-animation

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const shark = document.getElementById("shark");
+const sharkFrames = [
+  "img/1.Sharkie/3.Swim/1.png",
+  "img/1.Sharkie/3.Swim/2.png",
+  "img/1.Sharkie/3.Swim/3.png",
+  "img/1.Sharkie/3.Swim/4.png",
+  "img/1.Sharkie/3.Swim/5.png",
+  "img/1.Sharkie/3.Swim/6.png"
+];
+
+let currentFrame = 0;
+
+setInterval(() => {
+  shark.style.backgroundImage = `url(${sharkFrames[currentFrame]})`;
+  currentFrame = (currentFrame + 1) % sharkFrames.length;
+}, 150);
