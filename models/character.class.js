b4ad87ccfc313d0e480ec25a478afd494770c655
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2026-01-14 13:52:55 +0100

    fix: remove leading slash from character idle image paths
    Fixed character not appearing by correcting image path format.

--- models/character.class.js
+++ models/character.class.js
@@ -22,24 +22,24 @@ class Character extends MovableObject {
     currentHurtImages = null;
     hurtAnimationFrame = 0;
     IMAGES_IDLE = [
-        '/img/1.Sharkie/1.IDLE/1.png',
-        '/img/1.Sharkie/1.IDLE/2.png',
-        '/img/1.Sharkie/1.IDLE/3.png',
-        '/img/1.Sharkie/1.IDLE/4.png',
-        '/img/1.Sharkie/1.IDLE/5.png',
-        '/img/1.Sharkie/1.IDLE/6.png',
-        '/img/1.Sharkie/1.IDLE/7.png',
-        '/img/1.Sharkie/1.IDLE/8.png',
-        '/img/1.Sharkie/1.IDLE/9.png',
-        '/img/1.Sharkie/1.IDLE/10.png',
-        '/img/1.Sharkie/1.IDLE/11.png',
-        '/img/1.Sharkie/1.IDLE/12.png',
-        '/img/1.Sharkie/1.IDLE/13.png',
-        '/img/1.Sharkie/1.IDLE/14.png',
-        '/img/1.Sharkie/1.IDLE/15.png',
-        '/img/1.Sharkie/1.IDLE/16.png',
-        '/img/1.Sharkie/1.IDLE/17.png',
-        '/img/1.Sharkie/1.IDLE/18.png'
+        'img/1.Sharkie/1.IDLE/1.png',
+        'img/1.Sharkie/1.IDLE/2.png',
+        'img/1.Sharkie/1.IDLE/3.png',
+        'img/1.Sharkie/1.IDLE/4.png',
+        'img/1.Sharkie/1.IDLE/5.png',
+        'img/1.Sharkie/1.IDLE/6.png',
+        'img/1.Sharkie/1.IDLE/7.png',
+        'img/1.Sharkie/1.IDLE/8.png',
+        'img/1.Sharkie/1.IDLE/9.png',
+        'img/1.Sharkie/1.IDLE/10.png',
+        'img/1.Sharkie/1.IDLE/11.png',
+        'img/1.Sharkie/1.IDLE/12.png',
+        'img/1.Sharkie/1.IDLE/13.png',
+        'img/1.Sharkie/1.IDLE/14.png',
+        'img/1.Sharkie/1.IDLE/15.png',
+        'img/1.Sharkie/1.IDLE/16.png',
+        'img/1.Sharkie/1.IDLE/17.png',
+        'img/1.Sharkie/1.IDLE/18.png'
     ];
     IMAGES_LONG_IDLE = [
         'img/1.Sharkie/2.Long_IDLE/I1.png',
